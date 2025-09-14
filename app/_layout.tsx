132855dc18f51426266ce25f74751fc6d90406a2
M S Anjana <mahasathian.anjana@gmail.com>
2025-09-14 20:32:52 +0530

    Add new screens to RootLayout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,9 @@ export default function RootLayout() {
   return ( 
     <Stack>
       <Stack.Screen name="(tabs)/index" options={{ headerShown: false }} />
+      <Stack.Screen name="(tabs)/main_menu" options={{ headerShown: false }} />
+      <Stack.Screen name="(tabs)/agent_records_update" options={{ headerShown: false }} />
     </Stack>
+  
   );
 }
